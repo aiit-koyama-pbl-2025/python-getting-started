1f185babdbfc699986d982eafe3477c0b008e7ad
Kazuki Ogura <kazuki.ogura136@gmail.com>
2025-12-13 01:15:51 +0900

    fix2

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if IS_HEROKU_APP:
+if os.environ.get("DATABASE_URL"):
     # In production on Heroku the database configuration is derived from the `DATABASE_URL`
     # environment variable by the dj-database-url package. `DATABASE_URL` will be set
     # automatically by Heroku when a database addon is attached to your Heroku app. See:
